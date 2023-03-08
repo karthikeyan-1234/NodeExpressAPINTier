a29db32db73a15ad9cb909a936791cb068ff3c6e
karthikeyan-1234 <87603111+karthikeyan-1234@users.noreply.github.com>
2023-03-08 23:35:45 +0530

    Made changes to logging with Pino

--- logger.js
+++ logger.js
@@ -1,8 +1,9 @@
 const pino = require('pino');
 const logger = pino({
+  level: 'info',
     transport: {
       target: 'pino-pretty'
-    },
-   })
+    }
+   },pino.destination('./app.log'))
 
 module.exports = logger;
